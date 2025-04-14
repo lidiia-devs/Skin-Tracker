ac907fae57d0db94e19850c7de6a9c09d71fd4bd
Lidiia Diachkovskaia <lyatlasova@gmail.com>
2025-04-14 14:02:09 -0400

    Initial Commit

---
+++ Skin Tracker/Skin_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Skin_TrackerApp.swift
+//  Skin Tracker
+//
+//  Created by Lidiia Diachkovskaia on 4/14/25.
+//
+
+import SwiftUI
+
+@main
+struct Skin_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
